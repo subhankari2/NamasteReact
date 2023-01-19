63d6d271529034fd2af0b69d04e9f42ac1e06124
subhankari2 <padhisubhankari@gmail.com>
2023-01-19 18:30:26 +0530

    added keys to react elements, .babelrc to ignore console log , scripts to replace build cmd

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 const heading1=React.createElement("h1",{
     id:"title1",
-    style:{backgroundColor:"#0c2576",color:"white"}
+    style:{color:"white"},
+    key:"h1"
 },"Heading 1");
 const heading2=React.createElement("h1",{
     id:"title2",
-    style:{backgroundColor:"#0c2576",color:"white"}
+    style:{color:"white"},
+    key:"h2"
 },"Heading 2");
 const container=React.createElement("div",{id:"container"},[heading1,heading2]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
