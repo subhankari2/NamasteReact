8801ad06a3ebd026d183be830a8242a8c5741dc7
subhankari2 <padhisubhankari@gmail.com>
2023-01-19 20:23:05 +0530

    JSX  component composition

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-import React from "react"; 
 import ReactDOM  from "react-dom/client";
-console,console.log("inside ");
-const heading1=React.createElement("h1",{
-    id:"title1",
-    style:{color:"white"},
-    key:"h1"
-},"Heading 1");
-const heading2=React.createElement("h2",{
-    id:"title2",
-    style:{color:"white"},
-    key:"h2"
-},"Heading 2");
-const heading3=React.createElement("h3",{
-    id:"title3",
-    style:{color:"white"},
-    key:"h3"
-},"Heading 3");
 
-const container=React.createElement("div",{className:"title"},[heading1,heading2,heading3]);
+const heading1=()=>(
+    <h1 id="h1" key="h1">Heading1</h1>
+);
+const heading2=(
+    <h2 id="h2" key="h2">Heading2</h2>
+);
+const heading3=(
+    <h3 id="h3" key="h3">Heading2</h3>
+);
+const a=10;
+const FunctionalComponent=() =>{
+    return (
+    <div>
+        {heading1()}
+        {heading2}
+        {heading3}
+        {1+2}
+        {console.log(a)}
+        <h1>Namaste React Functional Component</h1>
+        </div>
+    );
+}
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// root.render( heading1());
+// root.render(FunctionalComponent());
+// root.render(<FunctionalComponent />);
+root.render(<FunctionalComponent></FunctionalComponent>);
