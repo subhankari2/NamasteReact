4f9b4ad8dbfd5c989072fbca86d441ae4e6a0035
subhankari2 <padhisubhankari@gmail.com>
2023-01-19 23:39:00 +0530

    Assignment chapter3

--- App.js
+++ App.js
@@ -1,30 +1,13 @@
 import ReactDOM  from "react-dom/client";
-
-const heading1=()=>(
-    <h1 id="h1" key="h1">Heading1</h1>
-);
-const heading2=(
-    <h2 id="h2" key="h2">Heading2</h2>
-);
-const heading3=(
-    <h3 id="h3" key="h3">Heading2</h3>
-);
-const a=10;
-const FunctionalComponent=() =>{
+const Header=() =>{
     return (
-    <div>
-        {heading1()}
-        {heading2}
-        {heading3}
-        {1+2}
-        {console.log(a)}
-        <h1>Namaste React Functional Component</h1>
+    <div className="headerDiv">
+        <div  className="logo" ></div>
+       <div className="searchBox"><div><input type="text"  placeholder="Seach "/></div></div>
+       <div  className="user" ></div>
         </div>
     );
 }
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-// root.render( heading1());
-// root.render(FunctionalComponent());
-// root.render(<FunctionalComponent />);
-root.render(<FunctionalComponent></FunctionalComponent>);
+root.render(<Header/>);
