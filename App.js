644be497f826a36bd370fbcd1bd8bc0a34f833cc
subhankari2 <padhisubhankari@gmail.com>
2023-01-19 19:16:15 +0530

    multiple child inside div element

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react"; 
 import ReactDOM  from "react-dom/client";
+console,console.log("inside ");
 const heading1=React.createElement("h1",{
     id:"title1",
     style:{color:"white"},
     key:"h1"
 },"Heading 1");
-const heading2=React.createElement("h1",{
+const heading2=React.createElement("h2",{
     id:"title2",
     style:{color:"white"},
     key:"h2"
 },"Heading 2");
-const container=React.createElement("div",{id:"container"},[heading1,heading2]);
+const heading3=React.createElement("h3",{
+    id:"title3",
+    style:{color:"white"},
+    key:"h3"
+},"Heading 3");
+
+const container=React.createElement("div",{className:"title"},[heading1,heading2,heading3]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
