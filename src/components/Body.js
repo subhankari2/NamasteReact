bc98fbabbfeb46e14550767c0a67ba32a840e8eb
subhankari2 <padhisubhankari@gmail.com>
2023-01-21 23:36:48 +0530

    Assignment chapter-05 search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,29 @@
 import { restaurants } from "../constants";
 import RestaurantCard  from "./RestaurantCard" 
 import { useState } from "react";
+function filterdata(searchText,restaurantList){
+ const filteredData= restaurantList.filter((restaurant)=>
+ restaurant.data.name.includes(searchText)
+);
+return filteredData;
+}
 const Body=() =>{
     // const searchText="KFC";
-const [searchText,setSearchText]=useState("KFC");
-const [searchBtn,setSearchBtn]=useState("false");
+const [searchText,setSearchText]=useState("");
+const [restaurantList,setRestaurants]=useState(restaurants);
     return (<>
     <div className="search-container">
 <input type="text" className="seachBox" value={searchText} 
 onChange={(e)=>{setSearchText(e.target.value);}}
 placeholder="search here" />
-<button>Search</button>
+<button onClick={()=>{
+ const data=filterdata(searchText,restaurantList);
+ setRestaurants(data);
+}}>Search</button>
 
-<h1>{searchBtn}</h1>
-<button onClick={(e)=>{
-    if(searchBtn==="true")
-    setSearchBtn("false");
-    else
-    setSearchBtn("true");
-}}>search</button>
     </div>
     <div className="restaurantList">
-    {restaurants.map((restaurant)=>{
+    {restaurantList.map((restaurant)=>{
 return<RestaurantCard {...restaurant.data}  key={restaurant.data.id} />
     })}
     </div>
